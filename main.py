c46afd2ceb58a8f404f41ca9d6e1080a41a82096
matvey <lepestock6@gmail.com>
2026-08-02 17:27:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import telebot
 from config import token
 from logic import send_picture_to_bot, send_mem_to_bot
-
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 bot = telebot.TeleBot(token)
+
+
 
 @bot.message_handler(commands=["start"])
 def start_bot(message):
@@ -28,4 +30,29 @@ def start_picture(message):
     with open(f"/mem/{send_picture_to_bot()}", "rb", encoding="utf-8") as photo:
     bot.send_message(message.chat.id, "Я отправляю тебе полезные и интересные мемы по теме Глобальному Потеплению")
 
+@bot.message_handler(commands=["test"])
+def start_bot(message):
+    bot.send_message(message.chat.id, "Привет! готов пройти тест по теме глобального потепления? ответь кнопками")
+
+def gen_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(InlineKeyboardButton("Yes", callback_data="cb_yes"),
+                               InlineKeyboardButton("No", callback_data="cb_no"))
+    return markup
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "cb_yes":
+        bot.answer_callback_query(call.id, "Answer is Yes")
+    elif call.data == "cb_no":
+        bot.answer_callback_query(call.id, "Answer is No")
+
+@bot.message_handler(func=lambda message: True)
+def message_handler(message):
+    bot.send_message(message.chat.id, "Yes/no?", reply_markup=gen_markup())
+
+
+
 bot.polling()
+
